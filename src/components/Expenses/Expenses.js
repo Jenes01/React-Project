808f317ec10857103829bd48149f77958cdea8ae
Jenes Moore <Jenes_M@hotmail.com>
2022-06-09 17:01:07 -0400

    Misspelled code fix

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,13 +8,13 @@ import "./Expenses.css";
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
 
-  const filteredExpenses = props.item.filter(expense => {
-     return expense.date.getFullYear().toString() === filteredYear;
-  });
-
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+ });
 
   return (
     <div>
